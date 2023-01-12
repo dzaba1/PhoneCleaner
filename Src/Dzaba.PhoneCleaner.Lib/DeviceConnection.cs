42c597d0add31b0de6325cbe71e56f3bd6f3fa81
Dzaba <blazej.zabialowicz@gmail.com>
2023-01-12 18:32:22 +0100

    Some logs

--- Src/Dzaba.PhoneCleaner.Lib/DeviceConnection.cs
+++ Src/Dzaba.PhoneCleaner.Lib/DeviceConnection.cs
@@ -1,4 +1,5 @@
 ﻿using MediaDevices;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,9 +10,15 @@ namespace Dzaba.PhoneCleaner.Lib
     internal sealed class DeviceConnection : IDeviceConnection
     {
         private readonly MediaDevice mediaDevice;
+        private readonly ILogger<DeviceConnection> logger;
 
-        public DeviceConnection(string friendlyName)
+        public DeviceConnection(string friendlyName,
+            ILogger<DeviceConnection> logger)
         {
+            Require.NotNull(logger, nameof(logger));
+
+            this.logger = logger;
+
             mediaDevice = MediaDevice.GetDevices()
                 .FirstOrDefault(x => x.FriendlyName == friendlyName);
 
@@ -20,14 +27,18 @@ namespace Dzaba.PhoneCleaner.Lib
                 throw new InvalidOperationException($"Could't find any devices with name '{friendlyName}'.");
             }
 
+            logger.LogInformation("Connecting to '{DeviceName}'", friendlyName);
             mediaDevice.Connect();
         }
+
+        public string FriendlyName => mediaDevice.FriendlyName;
 
         public void CopyFile(string source, Stream dest)
         {
             Require.NotWhiteSpace(source, nameof(source));
             Require.NotNull(dest, nameof(dest));
 
+            logger.LogInformation("Copy file from '{DeviceName}' '{Path}' to destination stream.", FriendlyName, source);
             mediaDevice.DownloadFile(source, dest);
         }
 
@@ -35,6 +46,7 @@ namespace Dzaba.PhoneCleaner.Lib
         {
             Require.NotWhiteSpace(path, nameof(path));
 
+            logger.LogInformation("Deleting directory at '{DeviceName}' '{Path}'. Recursive: {Recursive}.", FriendlyName, path, recursive);
             mediaDevice.DeleteDirectory(path, recursive);
         }
 
@@ -42,11 +54,13 @@ namespace Dzaba.PhoneCleaner.Lib
         {
             Require.NotWhiteSpace(path, nameof(path));
 
+            logger.LogInformation("Deleting file at '{DeviceName}' '{Path}'.", FriendlyName, path);
             mediaDevice.DeleteFile(path);
         }
 
         public void Dispose()
         {
+            logger.LogInformation("Disposing '{DeviceName}'", FriendlyName);
             mediaDevice?.Dispose();
         }
 
@@ -54,6 +68,7 @@ namespace Dzaba.PhoneCleaner.Lib
         {
             Require.NotWhiteSpace(path, nameof(path));
 
+            logger.LogInformation("Getting all directories from '{DeviceName}' '{Path}'. Search options: {SearchOption}.", FriendlyName, path, searchOption);
             return mediaDevice.EnumerateDirectories(path, "*", searchOption);
         }
 
@@ -61,6 +76,7 @@ namespace Dzaba.PhoneCleaner.Lib
         {
             Require.NotWhiteSpace(path, nameof(path));
 
+            logger.LogInformation("Getting all files from '{DeviceName}' '{Path}'. Search options: {SearchOption}.", FriendlyName, path, searchOption);
             return mediaDevice.EnumerateFiles(path, "*.*", searchOption);
         }
     }
