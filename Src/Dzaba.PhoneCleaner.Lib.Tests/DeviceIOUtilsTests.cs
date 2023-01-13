88c209812e5da62e660d1fa08b1a402d49d021f8
Dzaba <blazej.zabialowicz@gmail.com>
2023-01-13 20:05:49 +0100

    Getting directory or file names

---
+++ Src/Dzaba.PhoneCleaner.Lib.Tests/DeviceIOUtilsTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dzaba.PhoneCleaner.Lib.Tests
+{
+    [TestFixture]
+    public class DeviceIOUtilsTests
+    {
+        [TestCase(@"\\Phone\Dir", "Dir")]
+        [TestCase(@"\\Phone\Dir\", "Dir")]
+        [TestCase(@"\\Phone\", "Phone")]
+        [TestCase(@"\\Phone", "Phone")]
+        [TestCase(@"Phone", "Phone")]
+        [TestCase(@"Phone\", "Phone")]
+        [TestCase(@"\\Phone\Dir\test.txt", "test.txt")]
+        [TestCase(@"Phone\test.txt", "test.txt")]
+        public void GetFileOrDirectoryName_WhenPathProvided_ThenCorrectResult(string path, string expected)
+        {
+            var result = DeviceIOUtils.GetFileOrDirectoryName(path);
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Src/Dzaba.PhoneCleaner.Lib/DeviceIOUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dzaba.PhoneCleaner.Lib
+{
+    public static class DeviceIOUtils
+    {
+        public static string GetFileOrDirectoryName(string path)
+        {
+            Require.NotWhiteSpace(path, nameof(path));
+
+            var split = path.Split(new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return split.Last();
+        }
+    }
+}
