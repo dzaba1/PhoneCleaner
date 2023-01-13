7813ff814332f1c7e7d73d40f2b5a970c4a375d6
Dzaba <blazej.zabialowicz@gmail.com>
2023-01-13 21:02:38 +0100

    Copy action

---
+++ Src/Dzaba.PhoneCleaner.Lib.Tests/Handlers/CopyHandlerTests.cs
@@ -0,0 +1,150 @@
+﻿using AutoFixture;
+using Dzaba.PhoneCleaner.Lib.Config;
+using Dzaba.PhoneCleaner.Lib.Handlers;
+using FluentAssertions;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace Dzaba.PhoneCleaner.Lib.Tests.Handlers
+{
+    [TestFixture]
+    public class CopyHandlerTests : HandlerTestFixture
+    {
+        private CopyHandler CreateSut()
+        {
+            return Fixture.Create<CopyHandler>();
+        }
+
+        [Test]
+        public void Handle_WhenNotRecursive_ThenOnlyFilesAreCopied()
+        {
+            var model = new Copy()
+            {
+                Path = Path.Combine(DeviceRootDir, "Dir1"),
+                Destination = Path.Combine(WorkingDir, "Dir1"),
+                Override = true,
+                Recursive = false
+            };
+
+            var device = GetTempPathDevice();
+            SetupSomeDeviceFiles();
+
+            var sut = CreateSut();
+
+            var result = sut.Handle(model, device, GetCleanData());
+
+            result.Should().Be(1);
+            Directory.Exists(model.Destination).Should().BeTrue();
+            Directory.EnumerateFiles(model.Destination).Should().HaveCount(2);
+            Directory.EnumerateDirectories(model.Destination).Should().BeEmpty();
+        }
+
+        [Test]
+        public void Handle_WhenDirectoryDoesntExists_ThenNothing()
+        {
+            var model = new Copy()
+            {
+                Path = Path.Combine(DeviceRootDir, "Dir1"),
+                Destination = Path.Combine(WorkingDir, "Dir1"),
+                Override = true,
+                Recursive = false
+            };
+
+            var device = GetTempPathDevice();
+
+            var sut = CreateSut();
+
+            var result = sut.Handle(model, device, GetCleanData());
+
+            result.Should().Be(0);
+            Directory.Exists(model.Destination).Should().BeFalse();
+        }
+
+        [Test]
+        public void Handle_WhenFileExistButNotOverride_ThenFileIsNotCopied()
+        {
+            var expected = "Not touched";
+
+            var model = new Copy()
+            {
+                Path = Path.Combine(DeviceRootDir, "Dir1"),
+                Destination = Path.Combine(WorkingDir, "Dir1"),
+                Override = false,
+                Recursive = false
+            };
+
+            var device = GetTempPathDevice();
+            SetupSomeDeviceFiles();
+
+            var fileToCheck = Path.Combine(model.Destination, "file1.txt");
+            Directory.CreateDirectory(model.Destination);
+            File.WriteAllText(fileToCheck, expected);
+
+            var sut = CreateSut();
+
+            var result = sut.Handle(model, device, GetCleanData());
+
+            result.Should().Be(1);
+            Directory.EnumerateFiles(model.Destination).Should().HaveCount(2);
+            File.ReadAllText(fileToCheck).Should().Be(expected);
+        }
+
+        [Test]
+        public void Handle_WhenFileExistAndOverride_ThenFileIsCopied()
+        {
+            var expected = "Test";
+
+            var model = new Copy()
+            {
+                Path = Path.Combine(DeviceRootDir, "Dir1"),
+                Destination = Path.Combine(WorkingDir, "Dir1"),
+                Override = true,
+                Recursive = false
+            };
+
+            var device = GetTempPathDevice();
+            SetupSomeDeviceFiles();
+
+            var fileToCheck = Path.Combine(model.Destination, "file1.txt");
+            Directory.CreateDirectory(model.Destination);
+            File.WriteAllText(fileToCheck, "Not touched");
+
+            var sut = CreateSut();
+
+            var result = sut.Handle(model, device, GetCleanData());
+
+            result.Should().Be(1);
+            Directory.EnumerateFiles(model.Destination).Should().HaveCount(2);
+            File.ReadAllText(fileToCheck).Should().Be(expected);
+        }
+
+        [Test]
+        public void Handle_WhenRecursive_ThenDirectorisAreCopied()
+        {
+            var model = new Copy()
+            {
+                Path = Path.Combine(DeviceRootDir, "Dir1"),
+                Destination = Path.Combine(WorkingDir, "Dir1"),
+                Override = true,
+                Recursive = true
+            };
+
+            var device = GetTempPathDevice();
+            SetupSomeDeviceFiles();
+
+            var sut = CreateSut();
+
+            var result = sut.Handle(model, device, GetCleanData());
+
+            result.Should().Be(1);
+            Directory.Exists(model.Destination).Should().BeTrue();
+            Directory.EnumerateFiles(model.Destination).Should().HaveCount(2);
+            Directory.EnumerateDirectories(model.Destination).Should().HaveCount(2);
+
+            var nextDir = Directory.EnumerateDirectories(model.Destination).First();
+            Directory.EnumerateFiles(nextDir).Should().HaveCount(2);
+            Directory.EnumerateDirectories(nextDir).Should().HaveCount(2);
+        }
+    }
+}
---
+++ Src/Dzaba.PhoneCleaner.Lib/Handlers/CopyHandler.cs
@@ -0,0 +1,74 @@
+﻿using Dzaba.PhoneCleaner.Lib.Config;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Dzaba.PhoneCleaner.Lib.Handlers
+{
+    internal sealed class CopyHandler : HandlerBase<Copy>
+    {
+        private readonly ILogger<RemoveHandler> logger;
+
+        public CopyHandler(ILogger<RemoveHandler> logger)
+        {
+            Require.NotNull(logger, nameof(logger));
+
+            this.logger = logger;
+        }
+
+        protected override int Handle(Copy model, IDeviceConnection deviceConnection, CleanData cleanData)
+        {
+            Require.NotNull(model, nameof(model));
+            Require.NotNull(deviceConnection, nameof(deviceConnection));
+            Require.NotNull(cleanData, nameof(cleanData));
+
+            var root = deviceConnection.GetRootOrThrow(cleanData.DriveIndex);
+            var path = Path.Combine(root, model.Path);
+
+            var fullDest = Path.Combine(cleanData.WorkingDir, model.Destination);
+
+            logger.LogInformation("Invoking the copy action from '{Path}' to '{Destination}'. Recursive: {ContentRecursive}. Override: {Override}",
+                path, fullDest, model.Recursive, model.Override);
+
+            if (!deviceConnection.DirectoryExists(path))
+            {
+                logger.LogWarning("The directory '{Path}' doesn't exist.", path);
+                return 0;
+            }
+
+            CopyDirectory(deviceConnection, path, fullDest, model.Recursive, model.Override);
+            return 1;
+        }
+
+        private void CopyDirectory(IDeviceConnection deviceConnection,
+            string sourceDir, string destinationDir,
+            bool recursive, bool overwrite)
+        {
+            if (!Directory.Exists(destinationDir))
+            {
+                Directory.CreateDirectory(destinationDir);
+            }
+
+            foreach (var file in deviceConnection.EnumerateFiles(sourceDir, SearchOption.TopDirectoryOnly))
+            {
+                var fileName = DeviceIOUtils.GetFileOrDirectoryName(file);
+                string targetFilePath = Path.Combine(destinationDir, fileName);
+
+                if (overwrite || !File.Exists(targetFilePath))
+                {
+                    logger.LogInformation("Copy file '{Path}' to '{Destination}'.", file, targetFilePath);
+                    deviceConnection.CopyFile(file, targetFilePath, overwrite);
+                }
+            }
+
+            if (recursive)
+            {
+                foreach (var subDir in deviceConnection.EnumerateDirectories(sourceDir, SearchOption.TopDirectoryOnly))
+                {
+                    var subDirName = DeviceIOUtils.GetFileOrDirectoryName(subDir);
+                    string newDestinationDir = Path.Combine(destinationDir, subDirName);
+                    CopyDirectory(deviceConnection, subDir, newDestinationDir, true, overwrite);
+                }
+            }
+        }
+    }
+}
