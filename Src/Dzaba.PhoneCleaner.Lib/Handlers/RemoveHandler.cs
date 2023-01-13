6a7c32f7f3c328fcc4e4a5bd2309207e0025935a
Dzaba <blazej.zabialowicz@gmail.com>
2023-01-13 18:48:59 +0100

    Forgot about logging

--- Src/Dzaba.PhoneCleaner.Lib/Handlers/RemoveHandler.cs
+++ Src/Dzaba.PhoneCleaner.Lib/Handlers/RemoveHandler.cs
@@ -1,10 +1,21 @@
 ﻿using Dzaba.PhoneCleaner.Lib.Config;
+using Microsoft.Extensions.Logging;
 using System.IO;
+using System.Reflection;
 
 namespace Dzaba.PhoneCleaner.Lib.Handlers
 {
     internal sealed class RemoveHandler : HandlerBase<Remove>
     {
+        private readonly ILogger<RemoveHandler> logger;
+
+        public RemoveHandler(ILogger<RemoveHandler> logger)
+        {
+            Require.NotNull(logger, nameof(logger));
+
+            this.logger = logger;
+        }
+
         protected override int Handle(Remove model, IDeviceConnection deviceConnection, CleanData cleanData)
         {
             Require.NotNull(model, nameof(model));
@@ -13,6 +24,9 @@ namespace Dzaba.PhoneCleaner.Lib.Handlers
 
             var root = deviceConnection.GetRootOrThrow(cleanData.DriveIndex);
             var path = Path.Combine(root, model.Path);
+
+            logger.LogInformation("Invoking the remove action for path '{Path}'. Content flag: {Content}. Recursive: {Recursive}",
+                path, model.Content, model.Recursive);
 
             if (model.Content)
             {
