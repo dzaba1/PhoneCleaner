bed4297528c2bfb87e32328f809ec7d2661a9484
Dzaba <blazej.zabialowicz@gmail.com>
2023-01-13 18:58:51 +0100

    Better model

---
+++ Src/Dzaba.PhoneCleaner.Lib/Config/DirectoryAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Dzaba.PhoneCleaner.Lib.Config
+{
+    [Serializable]
+    public class DirectoryAction : Action
+    {
+        [XmlAttribute]
+        public bool Content { get; set; }
+
+        [XmlAttribute]
+        public bool ContentRecursive { get; set; }
+    }
+}
--- Src/Dzaba.PhoneCleaner.Lib/Config/Remove.cs
+++ Src/Dzaba.PhoneCleaner.Lib/Config/Remove.cs
@@ -4,15 +4,9 @@ using System.Xml.Serialization;
 namespace Dzaba.PhoneCleaner.Lib.Config
 {
     [Serializable]
-    public sealed class Remove : Action
+    public sealed class Remove : DirectoryAction
     {
         [XmlAttribute]
         public string Path { get; set; }
-
-        [XmlAttribute]
-        public bool Content { get; set; }
-
-        [XmlAttribute]
-        public bool ContentRecursive { get; set; }
     }
 }
