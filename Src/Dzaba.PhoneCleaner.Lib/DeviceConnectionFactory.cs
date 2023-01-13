6f578494e4dd6812e11df70e530476fb2b65437e
Dzaba <blazej.zabialowicz@gmail.com>
2023-01-13 21:37:52 +0100

    Device connection factory

---
+++ Src/Dzaba.PhoneCleaner.Lib/DeviceConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Dzaba.PhoneCleaner.Lib
+{
+    public interface IDeviceConnectionFactory
+    {
+        IDeviceConnection Create(string deviceName);
+    }
+
+    internal sealed class DeviceConnectionFactory : IDeviceConnectionFactory
+    {
+        private readonly ILoggerFactory loggerFactory;
+
+        public DeviceConnectionFactory(ILoggerFactory loggerFactory)
+        {
+            Require.NotNull(loggerFactory, nameof(loggerFactory));
+
+            this.loggerFactory = loggerFactory;
+        }
+
+        public IDeviceConnection Create(string deviceName)
+        {
+            Require.NotWhiteSpace(deviceName, nameof(deviceName));
+
+            return new DeviceConnection(deviceName, loggerFactory.CreateLogger<DeviceConnection>());
+        }
+    }
+}
